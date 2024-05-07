867c84ec199ed8dbe693be4f82e1287bc4775d3c
Blue Screen <159636015+0xBF90E913@users.noreply.github.com>
2024-05-08 00:47:46 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,12 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "openssl",
-            url: "https://github.com/passepartoutvpn/openssl-apple/releases/download/3.2.107/openssl.xcframework.zip",
-            checksum: "6aa5b917eb03487978b5d6ab1c019127af4f67568bd1f793c5b58cf786a49399"),
-
-        // local development
-//        .binaryTarget(
-//            name: "openssl",
-//            path: "frameworks/openssl.xcframework.zip")
+            url: "https://github.com/0xBF90E913/openssl-apple/releases/download/untagged-16ee942031888a9e8d3a/openssl.xcframework.zip",
+            checksum: "09fc269802b1f69cf09649542b374e0b175698e33d954111499b73498f7a010d"),
     ]
 )
