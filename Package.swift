fd1f1a8bbb1e8e1d1368bef21958f2fc3c8c451d
SNIPER <gametech.ir@gmail.com>
2025-02-24 00:43:57 +0330

    local

--- Package.swift
+++ Package.swift
@@ -13,9 +13,13 @@ let package = Package(
     dependencies: [
     ],
     targets: [
-        .binaryTarget(
-            name: "openssl",
-            url: "https://github.com/0xBF90E913/openssl-apple/releases/download/3.2.110/openssl.xcframework.zip",
-            checksum: "09fc269802b1f69cf09649542b374e0b175698e33d954111499b73498f7a010d"),
+        // .binaryTarget(
+        //     name: "openssl",
+        //     url: "https://github.com/0xBF90E913/openssl-apple/releases/download/3.2.110/openssl.xcframework.zip",
+        //     checksum: "09fc269802b1f69cf09649542b374e0b175698e33d954111499b73498f7a010d"),
+
+       .binaryTarget(
+           name: "openssl",
+           path: "openssl.xcframework.zip")
     ]
 )
