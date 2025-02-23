a018244b918cb2016d0e57fbc67e49ffc96bb6ae
SNIPER <gametech.ir@gmail.com>
2025-02-24 03:11:45 +0330

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,10 +3,6 @@
 
 import PackageDescription
 
-let filename = "openssl.xcframework.zip"
-let version = "3.4.200"
-let checksum = "0200db66d637586d74ca7380e968443cd6d289b24a247bf6270685145e176d9e"
-
 let package = Package(
     name: "openssl-apple",
     products: [
@@ -19,13 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "openssl",
-            url: "https://mmdsniper.ir/openssl/\(filename)",
-            checksum: checksum
-        ),
-
-        // local development
-//        .binaryTarget(
-//            name: "openssl",
-//            path: "frameworks/openssl.xcframework.zip")
+            url: "https://mmdsniper.ir/openssl/openssl.xcframework.zip",
+            checksum: "682574ccb4d42c721e1d38243d72d319c21005ed9e1bb5d4c8c2e2dd0e65aa15"),
     ]
 )
